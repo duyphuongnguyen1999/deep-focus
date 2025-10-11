507540694f722f060a1907038662720ad1d850be
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2025-10-11 20:26:39 +0700

    refactor: Split ESP32 firmware into smaller components

--- firmware/esp32/include/global.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
-
-#include <stdint.h>
-
-extern int16_t global_temperature;
-extern int16_t global_humidity;
-
-#endif
--- firmware/esp32/src/global.c
+++
@@ -1,4 +0,0 @@
-#include "global.h"
-
-int16_t global_temperature = 0x7FFF; // Use 0x7FFF to indicate uninitialized state
-int16_t global_humidity = 0x7FFF;    // Use 0x7FFF to indicate uninitialized state
