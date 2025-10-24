88723cbb34ab90c8bd69007b8bf711e02b68c80f
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2025-10-25 05:57:50 +0700

    feature: Init Controller Node ESP8266 feature

---
+++ firmware/nodes_platformio/src/platforms/controller_node_esp8266/controller_node_esp8266.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setupControllerNode()
+{
+    // Setup code for the controller node ESP8266
+    Serial.begin(115200);
+    Serial.println("Controller Node ESP8266 Initialized");
+}
+void loopControllerNode()
+{
+    // Main loop logic for the controller node
+}
