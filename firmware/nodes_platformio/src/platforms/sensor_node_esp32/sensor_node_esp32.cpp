e3a9c1ee5330d06946d7b22a84c92d94a4ecff19
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2025-10-25 05:58:28 +0700

    feature: Init Sensor Node ESP32 feature

---
+++ firmware/nodes_platformio/src/platforms/sensor_node_esp32/sensor_node_esp32.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setupSensorNode()
+{
+    Serial.begin(115200);
+    Serial.println("Sensor Node ESP32 Initialized");
+}
+
+void loopSensorNode()
+{
+    // Main loop logic for the sensor node
+}
