4acd4b378091fbb141b2afba327d270694539fcd
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2025-10-28 07:05:43 +0700

    feature: Add Analog Reader feature

---
+++ firmware/nodes_platformio/lib/analog_reader/analog_reader.cpp
@@ -0,0 +1,126 @@
+#include "analog_reader.h"
+
+AnalogReader::AnalogReader(
+    uint8_t pin,
+    uint16_t minAnalogValue,
+    uint16_t maxAnalogValue,
+    FilterMethod method,
+    uint32_t periodUs)
+    : _pin(pin),
+      _minAnalogValue(minAnalogValue),
+      _maxAnalogValue(maxAnalogValue),
+      _method(method),
+      _periodUs(periodUs),
+      _lastReadValue(0),
+      _windowSize(kDefaultWindowSize), // default
+      _smoothingFactor(kDefaultSmoothingFactor)
+{
+    // Ensure min < max
+    if (_minAnalogValue >= _maxAnalogValue)
+    {
+        _minAnalogValue = kDefaultMinAnalogValue;
+        _maxAnalogValue = kDefaultMaxAnalogValue;
+    }
+    switch (_method)
+    {
+    case FilterMethod::SMA:
+        _windowSize = kDefaultWindowSize;
+        _smaFilter.setWindowSize(_windowSize);
+        break;
+
+    case FilterMethod::EMA: // TODO Implement later
+    case FilterMethod::WMA: // TODO Implement later
+    case FilterMethod::NONE:
+    default:
+        _windowSize = 1;
+        break;
+    }
+}
+
+void AnalogReader::setFilterMethod(FilterMethod m)
+{
+    _method = m;
+}
+void AnalogReader::setWindowSize(uint8_t ws)
+{
+    _windowSize = ws;
+}
+void AnalogReader::setPeriodUs(uint32_t p)
+{
+    _periodUs = p;
+}
+void AnalogReader::setSmoothingFactor(float factor)
+{
+    _smoothingFactor = factor;
+}
+void AnalogReader::setAutoCal(bool autoCal)
+{
+    _autoCal = autoCal;
+}
+
+uint16_t AnalogReader::readRaw()
+{
+    const uint32_t now = micros();
+    if (now - _lastReadUs < _periodUs)
+        return _lastReadValue;
+    _lastReadUs = now;
+    _lastReadValue = analogRead(_pin);
+    return _lastReadValue;
+}
+
+uint16_t AnalogReader::readSmoothed()
+{
+    const uint16_t sample = AnalogReader::readRaw();
+    switch (_method)
+    {
+    case FilterMethod::SMA:
+        return _smaFilter.process(sample);
+
+    case FilterMethod::EMA: // TODO Implement later
+    case FilterMethod::WMA: // TODO Implement later
+    case FilterMethod::NONE:
+    default:
+        return sample;
+    }
+}
+
+float AnalogReader::readNormalized()
+{
+    const uint16_t sample = AnalogReader::readRaw();
+    float result;
+    if (_autoCal)
+    {
+        if (sample > _runMax)
+            _runMax = sample;
+        if (sample < _runMin)
+            _runMin = sample;
+        if (_runMax == _runMin)
+            return 0.0f;
+        result = static_cast<float>(sample - _runMin) / static_cast<float>(_runMax - _runMin);
+    }
+    else
+        result = static_cast<float>(sample - _minAnalogValue) / static_cast<float>(_maxAnalogValue - _minAnalogValue);
+    return result;
+}
+
+float AnalogReader::readNormalized(float upper, float lower)
+{
+    const float maxV = (upper > lower) ? upper : lower;
+    const float minV = (upper > lower) ? lower : upper;
+
+    if (maxV <= minV)
+        return 0.0f;
+
+    const uint16_t sample = AnalogReader::readRaw();
+
+    float result = (static_cast<float>(sample) - minV) /
+                   (maxV - minV);
+
+    return result;
+}
+
+float AnalogReader::readVoltage(float vref)
+{
+    const uint16_t adc = AnalogReader::readRaw();
+    return (static_cast<float>(adc) * vref) / 4095.0f;
+}
---
+++ firmware/nodes_platformio/lib/analog_reader/analog_reader.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <Arduino.h>
+#include "sma_filter.h"
+
+class AnalogReader
+{
+public:
+    enum class FilterMethod
+    {
+        SMA, // Simple Moving Average
+        EMA, // TODO Exponential Moving Average : Implements later
+        WMA, // TODO Weighted Moving Average : Implements later
+        NONE
+    };
+    // Default config
+    static constexpr uint16_t kDefaultMinAnalogValue = 0;
+    static constexpr uint16_t kDefaultMaxAnalogValue = 4095;
+    static constexpr FilterMethod kDefaultFilterMethod = FilterMethod::SMA;
+    static constexpr uint32_t kDefaultPeriodUs = 200000; // 200ms
+    static constexpr uint8_t kDefaultWindowSize = 10;
+    static constexpr float kDefaultSmoothingFactor = 0.2f;
+
+    explicit AnalogReader(
+        uint8_t pin,
+        uint16_t minAnalogValue = kDefaultMinAnalogValue,
+        uint16_t maxAnalogValue = kDefaultMaxAnalogValue,
+        FilterMethod method = kDefaultFilterMethod,
+        uint32_t periodUs = kDefaultPeriodUs);
+
+    // Setters / Getters
+    void setFilterMethod(FilterMethod m);
+    void setWindowSize(uint8_t ws);
+    void setPeriodUs(uint32_t period);
+    void setSmoothingFactor(float factor);
+    void setAutoCal(bool autoCal);
+
+    uint16_t readRaw();
+    uint16_t readSmoothed();
+    float readNormalized(); // Min-Max Normalizer
+    float readNormalized(float upper, float lower);
+    float readVoltage(float vref = 5.0f);
+
+private:
+    uint8_t _pin;
+    uint16_t _minAnalogValue, _maxAnalogValue;
+    FilterMethod _method;
+    uint8_t _windowSize;
+    float _smoothingFactor;
+    uint32_t _periodUs, _lastReadUs = 0;
+    uint16_t _lastReadValue = 0;
+    uint16_t _runMin = 4095;
+    uint16_t _runMax = 0;
+    bool _autoCal = false;
+
+    // SMA Filter
+    SMAFilter _smaFilter;
+};
